01b4beec4d94072e5448157c79f07692d72d90bb
tingting.gu <tingting.gu@gree.co.jp>
2015-12-09 18:33:44 +0900

    create the project and added a tableViewController

---
+++ Rabbit/Rabbit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rabbit
+//
+//  Created by Tingting Gu on 2015/12/09.
+//  Copyright © 2015年 Cookrin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Rabbit/Rabbit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Rabbit
+//
+//  Created by Tingting Gu on 2015/12/09.
+//  Copyright © 2015年 Cookrin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
